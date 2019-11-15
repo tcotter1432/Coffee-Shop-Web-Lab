dd5b3757d2e9512b11e014ab9aa9e24ac66f4484
tcotter1432 <tcotter1432@gmail.com>
2019-11-15 16:47:42 -0500

    First push

---
+++ Lab18/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayRegistrant(Registrant newRegistrant)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(newRegistrant);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Lab18/Models/Registrant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18.Models
+{
+    public class Registrant
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required]
+        [StringLength(48, MinimumLength = 8)]
+        public string Password { get; set; }
+        [Range(typeof(DateTime), "1/1/1900", "11/14/2001", ErrorMessage = "Age not Valid!")]
+        public DateTime DOB { get; set; }
+        
+
+    }
+}
